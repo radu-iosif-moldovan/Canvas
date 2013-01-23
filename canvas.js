5c05aa51f1adc0cf8105e218688e5d796a28413d
radu-iosif-moldovan <radu.iosif.moldovan@gmail.com>
2013-01-23 14:41:19 +0200

    with a square you can control

--- canvas.js
+++ canvas.js
@@ -99,10 +99,50 @@ paint.setGradient();
 }
 t.childNodes[31].onclick=function(){//set gradient color
 paint.setGradientColor(this.style.backgroundColor);
-
-
-
 }
+t.childNodes[33].onclick=function(e){//set gradient color
+c.width=c.width;
+c.style.backgroundColor ='#FFFFDD';
+var tile_size = 50;
+var startX = canvas.width / 2;
+var startY = canvas.height / 2;
+ctx.fillRect(startX, startY, tile_size, tile_size);  
+var cx = startX,
+    cy = startY; 
+
+document.addEventListener("keydown", function(e){ 
+    switch(e.keyCode)
+    {
+        //left
+        case 37:
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillRect(cx - tile_size, cy, tile_size, tile_size);
+                cx -= tile_size;
+        break;
+            
+        //up
+        case 38:
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillRect(cx, cy - tile_size, tile_size, tile_size);
+                cy -= tile_size;
+        break;
+            
+        //right
+        case 39:
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillRect(cx + tile_size, cy, tile_size, tile_size);
+                cx += tile_size;
+        break;
+        
+        //down
+        case 40:
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.fillRect(cx, cy + tile_size, tile_size, tile_size);
+                cy += tile_size;
+        break;
+    
+}
+})}
 
 
 c.onmousemove = function(evt){    
@@ -151,6 +191,12 @@ c.addEventListener('mouseup',function (evt){
     var r=Math.sqrt( ((paint.x-x)*(paint.x-x))+((paint.y-y)*(paint.y-y)) );
     ctx.arc(paint.x,paint.y,r,0,2*Math.PI);
     if (paint.fill){
+        if(paint.gradient){
+            var grad=ctx.createLinearGradient(x,y,paint.x,paint.y);
+            grad.addColorStop(0,paint.color);
+            grad.addColorStop(1,paint.gradientColor);
+            setFillStyle(grad);
+        }
     ctx.fill();
     }
     else{    
